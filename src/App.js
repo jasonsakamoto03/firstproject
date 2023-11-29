5cd159b81bb5df2a64dbee9f05e86b4ad29719f0
Jason Sakamoto <jasons03@uw.edu>
2023-11-28 17:38:13 -0800

    Ready to deploy site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+// import './App.css';
+// import ToDo from "./Widget";
+
+// function App() {
+
+//   const todos = ["Do HW", "Bake Cookies", "Shower"];
+
+//   return (
+//     <div className="App">
+//       <ul>
+//         {todos.map(todo => <ToDo text={todo} />)}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useState, useEffect } from "react";
+import Todo from "./Widget";
 import './App.css';
 
 function App() {
+
+const [todos, setTodos] = useState([]);
+const [input, setInput] = useState("");
+
+const updateInput = (e) => {
+  setInput(e.target.value);
+}
+
+const addTodo = () => {
+  if(input !== "") {
+    const updatedTodo = [...todos, input];
+    setTodos(updatedTodo);
+    setInput("");
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div style={{textAlign: "center"}}>
+    //   <Widget text="Web Impact is awesome!"/>
+    // </div>
+    <div className = "App">
+      <label htmlFor="newTodo">Enter new todo item:</label>
+      <input type="text" id="newTodo" value={input} onChange={updateInput}></input>
+      <button onClick={addTodo}>Enter</button>
+
+      <ul>
+        {todos.map((todo, index) => <Todo text={todo} />)}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Widget.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+
+// const ToDo = (props) => {
+//     return (
+//         <li>
+//             {props.text}
+//         </li>
+//     );
+// }
+
+// export default ToDo;
+import React from "react";
+
+const Todo = (props) => {
+    return (
+        <li>
+            {props.text}
+        </li>
+    )
+}
+
+export default Todo;
